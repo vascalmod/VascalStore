5b38376f48a6896ed82deff205bf4fa701103b50
vascalmod <vascalmod@users.noreply.github.com>
2026-05-06 22:31:54 +0800

    Update sw.js with Monetag service worker and add tag.min.js script to head

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,15 +1,6 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed')
-  self.skipWaiting()
-})
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker activated')
-  return self.clients.claim()
-})
-
-self.addEventListener('fetch', event => {
-  // Add any fetch event logic here if needed
-  // For now, just pass through all requests
-  event.respondWith(fetch(event.request))
-})
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10971885
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
